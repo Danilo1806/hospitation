510932ee0a1dddb10b8c111653b5bbc39f411619
frickeda <danilo.fricke@dataport.de>
2023-09-28 09:55:09 +0200

    added random list

--- hospitation.API/Controllers/FlachwitzController.cs
+++ hospitation.API/Controllers/FlachwitzController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hospitation.API.Controllers;
@@ -13,8 +14,19 @@ public class FlachwitzController : ControllerBase
     [HttpGet(Name = "flachwitz")]
     public IActionResult GetJoke()
     {
-        string Witz = "Dies ist ein Witz.";
-        return Ok(Witz);
+        List<String> flachwitze = new List<String>()
+                    {
+                        "Witz 1",
+                        "Witz 2",
+                        "Witz 3",
+                        "Witz 4",
+                        "Witz 5"                   
+                    };
+
+        Random rnd = new Random();
+        var index = rnd.Next(5);
+
+        return Ok(flachwitze[index]);
     }
 }
 
